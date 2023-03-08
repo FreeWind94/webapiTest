148d498a7eb1897da2cf2641346c0fb327ad493d
FreeWind94 <shukin-nikita@mail.ru>
2023-03-08 17:27:45 +0300

    Api with services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+global using webapiTest.Models;
+global using webapiTest.Services.CharacterService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
 
---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapiTest.Services.CharacterService;
+
+public interface ICharacterService
+{
+    List<Character> GetAllCharacters();
+    Character GetCharacterById(int id);
+    List<Character> AddCharacter(Character newCharacter);
+}
