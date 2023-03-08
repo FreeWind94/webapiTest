f9d52d166b6896d281ae1fcf6ae3098774b22fdc
FreeWind94 <shukin-nikita@mail.ru>
2023-03-08 17:39:08 +0300

    make async

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -7,7 +7,7 @@ namespace webapiTest.Services.CharacterService;
 
 public interface ICharacterService
 {
-    List<Character> GetAllCharacters();
-    Character GetCharacterById(int id);
-    List<Character> AddCharacter(Character newCharacter);
+    Task<List<Character>> GetAllCharacters();
+    Task<Character> GetCharacterById(int id);
+    Task<List<Character>> AddCharacter(Character newCharacter);
 }
